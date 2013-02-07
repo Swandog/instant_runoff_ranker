964357c607173ac8d8e3125404246ac36ba5d40e
Ken Swanson <kswanson@genome.wustl.edu>
2013-02-06 18:01:44 -0600

    Stubbed out method for actually building the ranking

--- lib/Ranking.pm
+++ lib/Ranking.pm
@@ -3,11 +3,16 @@ package Ranking;
 use Moose;
 
 use Voter;
+use List::AllUtils qw(min);
 
 has voters => (
                is       => 'ro',
                isa      => 'ArrayRef[Voter]',
+               traits   => ['Array'],
                required => 1,
+               handles  => {
+                            all_voters => 'elements',
+                           },
               );
 
 has ranking_order_ref => (
@@ -22,8 +27,52 @@ has ranking_order_ref => (
                                       },
                          );
 
+sub voter_ballots {
+    my $self=shift;
+
+    return
+      grep {$_}
+        map {$_->ballot}
+          $self->all_voters;
+}
+
 sub _build_ranking_order_ref {
-    return [];
+    my $self=shift;
+
+    my @ranking;
+    while(my @ballots=$self->voter_ballots) {
+        my $winner;
+        until($winner || ! @ballots) {
+            my %votes=
+              map {($_ => 0)}
+                map {$_->ranking}
+                  @ballots;
+            foreach my $ballot (@ballots) {
+                $votes{$ballot->top_choice}++;
+            }
+
+            foreach my $candidate (keys %votes) {
+                if($votes{$candidate}>@ballots/2) {
+                    $winner=$candidate;
+                }
+            }
+
+            unless($winner) {
+                my $loser_total=min values %votes;
+                my @losers=grep {$votes{$_}<=$loser_total} keys %votes;
+                foreach my $ballot (@ballots) {
+                    $ballot->eliminate(@losers);
+                }
+                @ballots=
+                  grep {$_->has_choices}
+                    @ballots;
+            }
+        }
+
+        push @ranking, $winner if($winner);
+    }
+
+    return \@ranking;
 }
 
 sub ranking_text {
